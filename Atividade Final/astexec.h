4ea7dcd7d308ae503e43fb87dc5b8e7490beb207
Jackson Gomes <jgg.snf@uea.edu.br>
2021-12-13 19:34:32 -0400

    add projeto final

---
+++ Atividade Final/astexec.h
@@ -0,0 +1,170 @@
+#ifndef ASTEXEC_H
+#define ASTEXEC_H
+
+#include "astgen.h"
+#include "hashtable.h"
+#include <assert.h>
+#include <math.h>
+
+int unaryOperator(char* op, int value);
+int operator(char* op, int left, int right);
+static void dispatchStatement(hashtable *htable, AstElement* node);
+double evalExpression(hashtable *htable, AstElement* node);
+static void execStmt(hashtable *htable, AstElement* a);
+
+double evalExpression(hashtable *htable, AstElement* node)
+{    
+    double left;
+    double right;
+    //printf("e-> %c \n", node->nodetype); 
+    Variable* variable = (Variable*)malloc(sizeof(Variable));
+
+    switch (node->nodetype)
+    { 
+
+    case 'Z':
+        return !evalExpression(htable, node->right); 
+        break; 
+
+    case 'V': 
+        return node->val;
+        break;
+
+    case 'N':        
+        return hash_lookup(htable,node->name)->value;
+        break;
+
+    case 'U':     
+        left =  evalExpression(htable, node->left);     
+        return unaryOperator(node->op, left);
+        break;
+
+    case 'E':   
+        left =  evalExpression(htable, node->left);         
+        right = evalExpression(htable, node->right);
+        return operator(node->op, left, right);
+    break;
+
+    default:
+        printf("expression not found %c \n", node->nodetype);
+    }
+    return 0;
+}
+
+static void dispatchStatement(hashtable *htable, AstElement* node)
+{
+    Variable* variable = (Variable*)malloc(sizeof(Variable));
+    double result;
+    //printf("d-> %c \n", node->nodetype); 
+    switch (node->nodetype)
+    {
+    case 'S':
+        execStmt(htable, node);
+        break;
+    case 'P':
+        printf("%s: %d\n", node->name, hash_lookup(htable, node->name)->value);
+        break;
+    case 'W':
+        printf("%s\n", node->name);
+        break;
+    case 'R':     
+        variable->name = node->name;
+        printf("Digite o valor de %s: \n", node->name);
+        scanf("%d", &variable->value );
+        hash_insert(htable, node->name, variable);
+        break;
+
+    case 'I':
+        result = evalExpression(htable, node->cond);
+        
+        if(result){            
+            execStmt(htable, node->left);         
+        }else{
+            if(node->right){
+                execStmt(htable, node->right);
+            }            
+        }
+        break;
+
+    case 'L':
+
+        while (evalExpression(htable, node->cond) == 1)
+        {
+            execStmt(htable, node->right);
+        }  
+
+    break;    
+
+    case 'D':
+        variable->name = node->name;
+        hash_insert(htable, node->name, variable);
+        break;
+
+    case 'A':               
+        variable->name = node->name;
+        variable->value = evalExpression(htable, node->right);
+        if(hash_update(htable, node->name,variable) == 0){
+            printf("Erro: variavel %s nao declarada \n", node->name);    
+        }
+        break;
+    case 'B':
+    default:
+        //printf("desconhecido \n");
+        break;
+    }
+}
+
+
+static void execStmt(hashtable *htable, AstElement* a)
+{    
+    for(int i = 0; i < a->count; i++)
+    {
+        dispatchStatement(htable, a->statements[i]);
+    }
+}
+
+//perdao
+int operator(char* op, int left, int right){
+    if (strcmp(op, "+") == 0){
+        return left + right;
+    }else if (strcmp(op, "-") == 0){
+        return left - right;
+    }else if (strcmp(op, "/") == 0){
+        return left / right;
+    }else if (strcmp(op, "*") == 0){
+        return left * right;
+    }else if (strcmp(op, ">=" )== 0){
+        return left >= right;
+    }else if (strcmp(op, "<=") == 0){
+        return left <= right;
+    }else if (strcmp(op, "!=") == 0){
+        return left != right;
+    }else if (strcmp(op, "<") == 0){
+        return left < right;
+    }else if (strcmp(op, ">") == 0){
+        return left > right;
+    }else if (strcmp(op, "==") == 0){
+        return left == right;
+    }else if (strcmp(op, "||") == 0){
+        return left || right;
+    }else if (strcmp(op, "&&") == 0){
+        return left && right;
+    }else if (strcmp(op, "%") == 0){
+        return (int)left % (int)right;
+    }else {
+        return 0;
+    }
+}
+
+int unaryOperator(char* op, int value){
+     if (strcmp(op, "-") == 0){
+        return - value;
+    }else if ((strcmp(op, "`") == 0)){
+        return sqrt(value);
+    }
+    else{
+        return 0;
+    }
+}
+
+#endif
---
+++ Atividade Final/paku.y
@@ -0,0 +1,118 @@
+%{
+#include <stdio.h>
+#include "hashtable.h"
+#include "astgen.h"
+extern FILE* yyin;
+
+void yyerror(struct AstElement** astDest, char *s);
+int yylex(void);
+int yyparse();
+
+extern int yylineno;
+hashtable *htable;
+%}
+
+%parse-param {struct AstElement** astDest}
+
+%union {
+    char *name;
+    double val;
+    char* op;
+    struct AstElement* ast;
+}
+
+%locations
+
+%token EQUALS EOL END
+%token P_LEFT P_RIGHT
+%token PRINT READ
+%token IF ELSE NOT WHILE
+%token O_KEY C_KEY 
+%token <name> STRING
+%left P_LEFT P_RIGHT
+
+%token <val> NUMBER 
+%token <name> IDENTIFIER TYPE
+%token <op> OPERATOR
+%type<ast> program block statement statements if_stmt attribution whileStmt print_stmt read_stmt declaration expression EOL
+
+
+%%
+
+program: statements { (*(struct AstElement**)astDest) = $1; YYACCEPT;};
+
+block: O_KEY EOL statements C_KEY { $$ = $3; };
+
+statements: {$$=0;}
+    | statements statement {$$=makeStatement($1, $2);}
+    ;
+
+statement:
+    declaration {$$=$1;}
+    | attribution {$$=$1;}
+    | if_stmt {$$=$1;}
+    | whileStmt {$$=$1;}
+    | read_stmt {$$=$1;}
+    | print_stmt {$$=$1;}
+    | block {$$=$1;}
+    | EOL {$$=makeBlank();}
+    ;
+
+if_stmt: IF P_LEFT expression P_RIGHT block EOL {$$=makeIf($3, $5, 0);}
+    | IF P_LEFT expression P_RIGHT block ELSE block EOL {$$=makeIf($3, $5, $7);}
+    ;
+
+whileStmt: WHILE P_LEFT expression P_RIGHT block EOL {$$=makeWhile($3, $5);};
+
+attribution: IDENTIFIER EQUALS expression EOL {$$=makeAssignment($1, $3);}
+    ;
+
+expression:
+    NUMBER {$$=makeExpByNum($1);}
+    | IDENTIFIER {$$=makeExpByName($1);}
+    | expression OPERATOR expression  {$$=makeExp($1, $3, $2);}
+    | P_LEFT expression P_RIGHT {$$=$2;}
+    | NOT expression {$$=makeNotExp($2);}
+    | OPERATOR expression {$$=makeUnaryExp($1,$2);}
+    ;
+
+print_stmt:
+    PRINT P_LEFT IDENTIFIER P_RIGHT EOL {$$=makePrint($3);}
+    | PRINT P_LEFT STRING P_RIGHT EOL {$$=makePrintString($3);}
+    ;
+read_stmt:
+    READ P_LEFT IDENTIFIER P_RIGHT EOL  {$$=makeRead($3);}
+    ;
+
+declaration:
+    TYPE IDENTIFIER EOL {$$=makeDeclaration($1,$2);}
+    ;
+
+%%
+
+#include "astexec.h"
+
+void yyerror(struct AstElement** astDest, char *s)
+{
+	printf("linha: %d :Error: %s\n", yylineno, s);
+}
+
+int main(int argc, char *argv[])
+{
+    struct AstElement *a = 0;
+    htable = hash_init(101);
+    // 1 para debug
+   /* #ifdef YYDEBUG
+        yydebug = 0;
+    #endif*/
+
+    int result;
+    yyin = fopen(argv[1], "r");
+    result = yyparse(&a);
+    if (result == 0){
+        printf("compilado com sucesso \n");
+        execStmt(htable, a);
+    }   
+    
+	return 0; 
+}
